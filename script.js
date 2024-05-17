eaefb3cc93887b1879c5427a568595c53e2c6f5c
Chimaobi Chinaka <chimaobi01@gmail.com>
2024-05-17 10:29:40 +0100

    Create calculator UI

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log('hello world');
+
+createButtons()
+
+function createButtons() {
+    const textContents = [
+        ['C', '+/-', '%', '/'],
+        [7, 8, 9, 'X'],
+        [4, 5, 6, '-'],
+        [1, 2, 3, '+'],
+        [0, ',', '<', '=']
+    ]
+    const buttons = document.querySelector('.buttons')
+    const row = 5
+    const column = 4
+    for (let i = 0; i < row; i++) {
+        const buttonRow = document.createElement('div')
+        for (let j = 0; j < column; j++) {
+            const button = document.createElement('button')
+            button.textContent = textContents[i][j]
+            buttonRow.appendChild(button)
+        }
+        buttons.appendChild(buttonRow)
+    }
+
+}
